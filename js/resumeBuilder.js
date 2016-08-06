7823dc4795598a6b7510e9400ab5805a89d0cfee
Tyler Pena <tylerpena@me.com>
2016-08-06 14:23:00 -0700

    Fixed review requests

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,13 +4,111 @@ var bio = {
     "contacts": [{
         "mobile": "925-813-2796",
         "email": "tylerpena@me.com",
-        "twitter": "@mrtpain_",
         "github": "mrtpain",
+        "twitter": "@mrtpain_",
         "location": "Oakley, CA"
     }],
-    "bioPic": 'images/me.jpg',
     "welcomeMessage": "Welcome to my resume where you can read all about my awesome ass skills. I've been developing for several years now and am excelling at a crazy pace. So crazy that before I finish projects, I already want to restart and build a newer, faster and better one.",
-    "skills": ["HTML5", "CSS3", "SASS", "jQuery", "JavaScript", "Craft CMS", "Foundation", "Bootstrap", "Gulp", "Twig", "Grunt"]
+    "skills": ["HTML5", "CSS3", "SASS", "jQuery", "JavaScript", "Craft CMS", "Foundation", "Bootstrap", "Gulp", "Twig", "Grunt"],
+    "biopic": 'images/me.jpg'
+};
+
+// DISPLAY BIO INFORMATION IN AN OBJECT THAT HOLDS A FUNCTION
+bio.display = function() {
+    // APPEND NAME, ROLE, PIC, AND WELCOME MESSAGE TO HEADER
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    var formattedPic = HTMLbioPic.replace('%data%', bio.biopic);
+    var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+
+    $('#header').prepend(formattedRole);
+    $('#header').prepend(formattedName);
+    $('#header').append(formattedPic);
+    $('#header').append(formattedWelcome);
+
+    // APPEND CONTACTS TO HEADER
+    for (var i = 0; i < bio['contacts'].length; i++) {
+        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[i].mobile);
+        var formattedEmail = HTMLemail.replace("%data%", bio.contacts[i].email);
+        var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts[i].twitter);
+        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts[i].github);
+        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts[i].location);
+        $('#topContacts, #footerContacts').append(formattedMobile);
+        $('#topContacts, #footerContacts').append(formattedEmail);
+        $('#topContacts, #footerContacts').append(formattedTwitter);
+        $('#topContacts, #footerContacts').append(formattedGithub);
+        $('#topContacts, #footerContacts').append(formattedLocation);
+    }
+
+    // APPEND SKILLS TO HEADER
+    $('#header').append(HTMLskillsStart);
+    //for (skill in bio.skills) {
+    for (var i = 0; i < bio['skills'].length; i++) {
+        var skillItem = HTMLskills.replace("%data%", bio.skills[i]);
+        $('#skills').append(skillItem);
+    }
+};
+
+var education = {
+    "schools": [{
+        "name": "Los Medanos College",
+        "location": "Antioch, CA",
+        "degree": "N/A",
+        "majors": "N/A",
+        "dates": "2012",
+        "url": "http://www.losmedanos.edu/"
+
+    }, {
+        "name": "Kaplan University",
+        "location": "Davenport, IA",
+        "degree": "Bachelors of Science in Information Technology",
+        "majors": "Computer Science",
+        "dates": "2012 - 2015",
+        "url": "http://kaplanuniversity.edu"
+    }],
+    "onlineCourses": [{
+        "title": "Front End Developer Nanodegree",
+        "school": "Udacity",
+        "dates": "In Progress",
+        "url": "https://udacity.com"
+    }]
+};
+
+// Need to apply URL for links
+education.display = function() {
+    for (var i = 0; i < education['schools'].length; i++) {
+        $('#education').append(HTMLschoolStart);
+
+        var formattedName = HTMLschoolName.replace('%data%', education.schools[i].name);
+        var formattedUrl = formattedName.replace('#', education.schools[i].url);
+        var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[i].degree);
+        $('.education-entry:last').append(formattedUrl + formattedDegree);
+
+        var formattedDates = HTMLschoolDates.replace('%data%', education.schools[i].dates);
+        $('.education-entry:last').append(formattedDates);
+        var formattedLocation = HTMLschoolLocation.replace('%data%', education.schools[i].location);
+        $('.education-entry:last').append(formattedLocation);
+
+        var formattedMajors = HTMLschoolMajor.replace('%data%', education.schools[i].majors);
+        $('.education-entry:last').append(formattedMajors);
+    }
+
+    for (var i = 0; i < education['onlineCourses'].length; i++) {
+        $('#education').append(HTMLonlineClasses);
+        $('#education').append(HTMLschoolStart);
+
+        var formattedCourseName = HTMLonlineTitle.replace('%data%', education.onlineCourses[i].title);
+        var formattedCourseUrl = formattedCourseName.replace('#', education.onlineCourses[i].url);
+        var formattedSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[i].school);
+        $('.education-entry:last').append(formattedCourseUrl + formattedSchool);
+
+        var formattedCourseDates = HTMLonlineDates.replace('%data%', education.onlineCourses[i].dates);
+        $('.education-entry:last').append(formattedCourseDates);
+
+        var formattedURL = HTMLonlineURL.replace('%data%', education.onlineCourses[i].url);
+        var formattedLink = formattedURL.replace("#", education.onlineCourses[i].url);
+        $('.education-entry:last').append(formattedLink);
+    }
 };
 
 var work = {
@@ -35,153 +133,17 @@ var work = {
     }]
 };
 
-var education = {
-    "schools": [{
-        "name": "Los Medanos College",
-        "location": "Antioch, CA",
-        "degree": "N/A",
-        "major": "N/A",
-        "dates": "2012"
-
-    }, {
-        "name": "Kaplan University",
-        "location": "Davenport, IA",
-        "degree": "Bachelors of Science in Information Technology",
-        "major": "Computer Science",
-        "dates": "2012 - 2015"
-    }],
-    "onlineCourses": [{
-        "title": "Front End Developer Nanodegree",
-        "school": "Udacity",
-        "dates": "In Progress",
-        "url": "https://udacity.com"
-    }]
-};
-
-var projects = {
-    "projects": [{
-        "title": "Portfolio website",
-        "date": "August 2016",
-        "description": "This was done for the first project for the Udacity Front End Dev Nanodegree. I used Zurb's Foundation for the grid framework and gulp for sass to minified css. I have a much more advanced personal website in development, I chose to meet the minimum requirements for this project to move forward with others.",
-        "images": ['images/portfolio.jpg', 'images/portfolio2.jpg']
-    }, {
-        "title": "Frogger Game",
-        "date": "August 2016",
-        "description": "This is for the 3rd project in the Udacity Front End Dev Nanodegree. I have not yet completed this project but it is in progress currently. This project uses OOP to complete a Frogger like game. This project's difficulty level stepped up big time from other past projects.",
-        "images": ['images/frogger.jpg']
-    }, {
-        "title": "ThermoFusion",
-        "date": "Launched May 2016",
-        "description": "ThermoFusion is a Heat Treating and Brazing company in Hayward, Ca. They need a new website to feature their company and their focus on a strong customer experience. This was an awesome project to work on with the complex design and freedom from Wordpress a template. Instead, I chose to go with a newer CMS called Craft.",
-        "images": ['images/thermofusion.jpg', 'images/thermofusion2.jpg', 'images/thermofusion3.jpg']
-    }]
-};
-
-// DISPLAY PROJECTS INFORMATION IN AN OBJECT THAT HOLDS A FUNCTION
-projects.display = function() {
-    for (project in projects.projects) {
-        $('#projects').append(HTMLprojectStart);
-
-        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-        $('.project-entry:last').append(formattedTitle);
-
-        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].date);
-        $('.project-entry:last').append(formattedDates);
-
-        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-        $('.project-entry:last').append(formattedDescription);
-
-        if (projects.projects[project].images.length > 0) {
-            for (image in projects.projects[project].images) {
-                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-                $('.project-entry:last').append(formattedImage);
-            }
-        }
-    }
-};
-
-// Need to apply URL for links
-education.display = function() {
-    for (school in education.schools) {
-        $('#education').append(HTMLschoolStart);
-
-        var formattedName = HTMLschoolName.replace('%data%', education.schools[school].name);
-        var formattedDegree = HTMLschoolDegree.replace('%data%', education.schools[school].degree);
-        $('.education-entry:last').append(formattedName + formattedDegree);
-
-        var formattedDates = HTMLschoolDates.replace('%data%', education.schools[school].dates);
-        $('.education-entry:last').append(formattedDates);
-        var formattedLocation = HTMLschoolLocation.replace('%data%', education.schools[school].location);
-        $('.education-entry:last').append(formattedLocation);
-
-        var formattedMajors = HTMLschoolMajor.replace('%data%', education.schools[school].major);
-        $('.education-entry:last').append(formattedMajors);
-    }
-
-    for (course in education.onlineCourses) {
-        $('#education').append(HTMLonlineClasses);
-        $('#education').append(HTMLschoolStart);
-
-        var formattedCourseName = HTMLonlineTitle.replace('%data%', education.onlineCourses[course].title);
-        var formattedSchool = HTMLonlineSchool.replace('%data%', education.onlineCourses[course].school);
-        $('.education-entry:last').append(formattedCourseName + formattedSchool);
-
-        var formattedCourseDates = HTMLonlineDates.replace('%data%', education.onlineCourses[course].dates);
-        $('.education-entry:last').append(formattedCourseDates);
-
-        var formattedURL = HTMLonlineURL.replace('%data%', education.onlineCourses[course].url);
-        var formattedLink = formattedURL.replace("#", education.onlineCourses[course].url);
-        $('.education-entry:last').append(formattedLink);
-    }
-};
-
-// DISPLAY BIO INFORMATION IN AN OBJECT THAT HOLDS A FUNCTION
-bio.display = function() {
-    // APPEND NAME, ROLE, PIC, AND WELCOME MESSAGE TO HEADER
-    var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    var formattedPic = HTMLbioPic.replace('%data%', bio.bioPic);
-    var formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
-
-    $('#header').prepend(formattedRole);
-    $('#header').prepend(formattedName);
-    $('#header').append(formattedPic);
-    $('#header').append(formattedWelcome);
-
-
-    // APPEND CONTACTS TO HEADER
-    for (contact in bio.contacts) {
-        var formattedMobile = HTMLmobile.replace("%data%", bio.contacts[contact].mobile);
-        var formattedEmail = HTMLemail.replace("%data%", bio.contacts[contact].email);
-        var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts[contact].twitter);
-        var formattedGithub = HTMLgithub.replace("%data%", bio.contacts[contact].github);
-        var formattedLocation = HTMLlocation.replace("%data%", bio.contacts[contact].location);
-        $('#topContacts, #footerContacts').append(formattedMobile);
-        $('#topContacts, #footerContacts').append(formattedEmail);
-        $('#topContacts, #footerContacts').append(formattedTwitter);
-        $('#topContacts, #footerContacts').append(formattedGithub);
-        $('#topContacts, #footerContacts').append(formattedLocation);
-    }
-
-    // APPEND SKILLS TO HEADER
-    $('#header').append(HTMLskillsStart);
-    for (skill in bio.skills) {
-        var skillItem = HTMLskills.replace("%data%", bio.skills[skill]);
-        $('#skills').append(skillItem);
-    }
-};
-
 // DISPLAY WORK EXPERIENCE IN A FUNCTION RATHER THAN AN OBJECT
 work.display = function() {
-    for (job in work.jobs) {
+    for (var i = 0; i < work['jobs'].length; i++) {
         $('#workExperience').append(HTMLworkStart);
 
-        var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
-        var formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+        var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[i].employer);
+        var formattedTitle = HTMLworkTitle.replace('%data%', work.jobs[i].title);
         var formattedEmployerTitle = formattedEmployer + formattedTitle;
-        var formattedDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
-        var formattedLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
-        var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+        var formattedDates = HTMLworkDates.replace('%data%', work.jobs[i].dates);
+        var formattedLocation = HTMLworkLocation.replace('%data%', work.jobs[i].location);
+        var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[i].description);
 
         $(".work-entry:last").append(formattedEmployerTitle);
         $(".work-entry:last").append(formattedDates);
@@ -189,6 +151,56 @@ work.display = function() {
         $(".work-entry:last").append(formattedDescription);
     }
 };
+
+var projects = {
+    "projects": [{
+        "title": "Portfolio website",
+        "dates": "August 2016",
+        "description": "This was done for the first project for the Udacity Front End Dev Nanodegree. I used Zurb's Foundation for the grid framework and gulp for sass to minified css. I have a much more advanced personal website in development, I chose to meet the minimum requirements for this project to move forward with others.",
+        "images": ['images/portfolio.jpg', 'images/portfolio2.jpg']
+    }, {
+        "title": "Frogger Game",
+        "dates": "August 2016",
+        "description": "This is for the 3rd project in the Udacity Front End Dev Nanodegree. I have not yet completed this project but it is in progress currently. This project uses OOP to complete a Frogger like game. This project's difficulty level stepped up big time from other past projects.",
+        "images": ['images/frogger.jpg']
+    }, {
+        "title": "ThermoFusion",
+        "dates": "Launched May 2016",
+        "description": "ThermoFusion is a Heat Treating and Brazing company in Hayward, Ca. They need a new website to feature their company and their focus on a strong customer experience. This was an awesome project to work on with the complex design and freedom from Wordpress a template. Instead, I chose to go with a newer CMS called Craft.",
+        "images": ['images/thermofusion.jpg', 'images/thermofusion2.jpg', 'images/thermofusion3.jpg']
+    }]
+};
+
+// DISPLAY PROJECTS INFORMATION IN AN OBJECT THAT HOLDS A FUNCTION
+projects.display = function() {
+    for (var i = 0; i < projects['projects'].length; i++) {
+        $('#projects').append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+        $('.project-entry:last').append(formattedTitle);
+
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+        $('.project-entry:last').append(formattedDates);
+
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+        $('.project-entry:last').append(formattedDescription);
+
+        if (projects.projects[i].images.length > 0) {
+            for (var image = 0; image < projects.projects[i].images.length; image++) {
+                var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[image]);
+                $('.project-entry:last').append(formattedImage);
+            }
+        }
+    }
+};
+
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+$('#mapDiv').append(googleMap);
+$('#main').append(internationalizeButton);
 
 // CHANGE LAST NAME TO UPPERCASE
 function inName(name) {
@@ -200,19 +212,6 @@ function inName(name) {
     return name[0] + " " + name[1];
 }
 
-
-
-bio.display();
-work.display();
-projects.display();
-education.display();
-
-$('#mapDiv').append(googleMap);
-$('#main').append(internationalizeButton);
-
-
-
-
 // RECORD CLICK LOCATION
 $(document).click(function(loc) {
     var x = loc.pageX;
@@ -220,7 +219,6 @@ $(document).click(function(loc) {
 
     logClicks(x, y);
 });
-
 
 // GRAB LOCATIONS OF JOBS
 function locationizer(work_obj) {
@@ -234,4 +232,4 @@ function locationizer(work_obj) {
     return locationArray;
 }
 
-console.log(locationizer(work));
+//console.log(locationizer(work));
